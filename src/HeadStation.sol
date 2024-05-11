69e55a2e207801499bde31c6ccf4bd1bf6f8857b
Karthikeya.Gundumogula <57733888+KarthikeyaGundumogula@users.noreply.github.com>
2024-05-11 13:56:30 +0000

    update

--- src/HeadStation.sol
+++ src/HeadStation.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.20;
 
 contract HeadStation {
   
+  mapping(address user => uint256 kelCoinBalance) private s_balances;
+  
 }
--- src/TellerStation.sol
+++ src/TellerStation.sol
@@ -27,12 +27,25 @@ interface HeadStation {
     function move(address, address, uint) external;
 }
 
+/**
+ * @notice this Contract handles the deposit and witdrawl of Kel Coin 
+ */
 contract KelTellerStation {
     error KelTellerStationError_UnAuthorizedOperation();
+    error KelTellerStationError_PausedWithdrawls(); 
 
+    uint256 private constant HEADSTATIONPRESICION = 10**27;
     KelCoin public kelCoin;
     HeadStation public headStation;
+    bool public status;
     mapping(address user => bool authorized) private s_authorizedAddresses;
+
+    //--Events--//
+    event AuthorizedAddressAdded(address user);
+    event AuthorizedAddressRemoved(address user);
+    event KelCoinDeposited(address user, uint amount);
+    event KelCoinWithdrawn(address user, uint amount);
+    event StausUpdated(bool status);
 
     constructor(address _kelCoin, address _headStation) {
         kelCoin = KelCoin(_kelCoin);
@@ -40,8 +53,13 @@ contract KelTellerStation {
         s_authorizedAddresses[msg.sender] = true;
     }
 
-    //--Authorization--//
+    //--Internal Helpers--//
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        z = x * y;
+        require(y == 0 || z / y == x);
+    }
 
+    //--Authorization & Administration--//
     modifier authorize() {
         if (s_authorizedAddresses[msg.sender] != true) {
             revert KelTellerStationError_UnAuthorizedOperation();
@@ -51,4 +69,29 @@ contract KelTellerStation {
     function addAuthorizedAddress(address user) external authorize {
         s_authorizedAddresses[user] = true;
     }
+    function removeAuthorizedAddress(address user) external authorize {
+        s_authorizedAddresses[user] = false;
+    }
+    function changeStatus() external authorize {
+        status = !status;
+        emit StausUpdated(status);
+    }
+
+    //--Deposit or Withdraw Kel Coin--//
+
+    function depositKelCoin(address _user, uint _amount) external {
+        headStation.move(address(this), _user, multiply(_amount, HEADSTATIONPRESICION));
+        kelCoin.burn(msg.sender, _amount);
+        emit KelCoinDeposited(_user, _amount);
+    }
+
+    function withdrawKelCoin(address _user, uint _amount) external {
+        if(status == false) {
+            revert KelTellerStationError_PausedWithdrawls();
+        }
+        headStation.move(_user, address(this), multiply(_amount, HEADSTATIONPRESICION));
+        kelCoin.mint(msg.sender, _amount);
+        emit KelCoinWithdrawn(_user, _amount);
+    }
 }
+
