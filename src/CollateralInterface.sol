558d74232f0d167f0b2ff1da848e16167f1cc35d
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2024-06-01 11:35:23 +0000

    resolved all the errors, implemented keeper model

--- src/CollateralInterface.sol
+++ src/CollateralInterface.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
+
+/**
+ * @title CollateralInterface
+ * @author Karthikeya Gundumogula
+ * @notice This contract used to deposit and withdraw collateral on the other chains
+ */
 pragma solidity 0.8.19;
 
 import {IRouterClient} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/interfaces/IRouterClient.sol";
@@ -10,153 +16,189 @@ import {SafeERC20} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/vendor/openzep
 
 using SafeERC20 for IERC20;
 
-contract MessageTracker is CCIPReceiver, OwnerIsCreator {
-    
-    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); 
-    error NothingToWithdraw(); 
+contract CollateralInterface is CCIPReceiver, OwnerIsCreator {
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+    error CollateralTokenError_InsufficientCollateralBalance();
+    error CollateralInterfaceError_SenderNotAllowedThisContractToTransferFunds();
+    error CollateralInterfaceError_InsufficientCollateralBalance();
+    error NothingToWithdraw();
     error DestinationChainNotAllowlisted(uint64 destinationChainSelector);
     error SourceChainNotAllowlisted(uint64 sourceChainSelector);
     error SenderNotAllowlisted(address sender);
-    error InvalidReceiverAddress(); 
-    error MessageWasNotSentByMessageTracker(bytes32 msgId); 
+    error InvalidReceiverAddress();
+    error MessageWasNotSentByMessageTracker(bytes32 msgId);
     error MessageHasAlreadyBeenProcessedOnDestination(bytes32 msgId);
 
     enum MessageStatus {
-        NotSent, 
-        Sent, 
-        ProcessedOnDestination 
+        NotSent,
+        Sent,
+        ProcessedOnDestination
     }
 
-    struct MessageInfo {
+    struct Transaction {
+        string txType;
+        address user;
+        uint256 amount;
         MessageStatus status;
         bytes32 acknowledgerMessageId;
     }
 
-    mapping(uint64 => bool) public allowlistedDestinationChains;
-
-    mapping(uint64 => bool) public allowlistedSourceChains;
-
-    mapping(address => bool) public allowlistedSenders;
-
-    mapping(bytes32 => MessageInfo) public messagesInfo;
+    string private constant WITHDRAW = "W";
+    string private constant DEPOSIT = "D";
+    string private constant DEPOSITSUCCESS = "DS";
+    string private constant DEPOSITFAIL = "DF";
+    string private constant WITHDRAWSUCCESS = "WS";
+    string private constant WITHDRAWFAIL = "WF";
+    address public s_receivingContractAddress;
+    uint64 public s_receivingChainId;
+    mapping(bytes32 => Transaction) public s_transactionsInfo;
+    mapping(address user => uint256 amount) public s_userBalances;
+    mapping(address user => uint256 amount) public s_failedDeposits;
+    mapping(address user => uint256 amount) public s_acceptedWithdrawls;
 
     event MessageSent(
-        bytes32 indexed messageId, 
-        uint64 indexed destinationChainSelector,
-        address receiver,
-        string text, 
-        address feeToken,
-        uint256 fees 
+        bytes32 indexed messageId,
+        uint256 amount,
+        address sender,
+        string operation
     );
     event MessageProcessedOnDestination(
-        bytes32 indexed messageId,     
-        bytes32 indexed acknowledgedMsgId,
-        uint64 indexed sourceChainSelector,
-        address sender 
+        bytes32 indexed messageId,
+        string status
     );
+    event DepositSuccess(
+        bytes32 indexed messageId,
+        address user,
+        uint256 amount
+    );
+    event DepositFailed(
+        bytes32 indexed messageId,
+        address user,
+        uint256 amount
+    );
+    event WithdrawRequestSuccess(
+        bytes32 indexed messageId,
+        address user,
+        uint256 amount
+    );
+    event WithdrawRequestFailed(
+        bytes32 indexed messageId,
+        address user,
+        uint256 amount
+    );
+    event CollateralWithdrawn(address user, uint256 amount);
+    event FailedDepositWithdraw(address user, uint256 amount);
 
     IERC20 private s_linkToken;
+    IERC20 private s_collateralToken;
 
-    constructor(address _router, address _link) CCIPReceiver(_router) {
+    constructor(
+        address _router,
+        address _link,
+        address _collateralToken
+    ) CCIPReceiver(_router) {
         s_linkToken = IERC20(_link);
+        s_collateralToken = IERC20(_collateralToken);
     }
 
-    modifier onlyAllowlistedDestinationChain(uint64 _destinationChainSelector) {
-        if (!allowlistedDestinationChains[_destinationChainSelector])
-            revert DestinationChainNotAllowlisted(_destinationChainSelector);
-        _;
-    }
-    modifier onlyAllowlisted(uint64 _sourceChainSelector, address _sender) {
-        if (!allowlistedSourceChains[_sourceChainSelector])
-            revert SourceChainNotAllowlisted(_sourceChainSelector);
-        if (!allowlistedSenders[_sender]) revert SenderNotAllowlisted(_sender);
-        _;
-    }
-    modifier validateReceiver(address _receiver) {
-        if (_receiver == address(0)) revert InvalidReceiverAddress();
-        _;
-    }
-    function allowlistDestinationChain(
-        uint64 _destinationChainSelector,
-        bool allowed
+    function initReceiver(
+        address _recevierContract,
+        uint64 _receiverChainId
     ) external onlyOwner {
-        allowlistedDestinationChains[_destinationChainSelector] = allowed;
+        s_receivingContractAddress = _recevierContract;
+        s_receivingChainId = _receiverChainId;
     }
-    function allowlistSourceChain(
-        uint64 _sourceChainSelector,
-        bool allowed
-    ) external onlyOwner {
-        allowlistedSourceChains[_sourceChainSelector] = allowed;
+
+    /**
+     * @dev contract needs to be approved from the msg.sender to transfer collateral into this contract
+     */
+
+    function depositCollateral(uint256 _amount) external {
+        if (s_collateralToken.balanceOf(msg.sender) < _amount) {
+            revert CollateralTokenError_InsufficientCollateralBalance();
+        }
+        if (s_collateralToken.allowance(msg.sender, address(this)) < _amount) {
+            revert CollateralInterfaceError_SenderNotAllowedThisContractToTransferFunds();
+        }
+        s_collateralToken.transferFrom(msg.sender, address(this), _amount);
+        sendMessage(msg.sender, _amount, DEPOSIT);
     }
-    function allowlistSender(address _sender, bool allowed) external onlyOwner {
-        allowlistedSenders[_sender] = allowed;
+
+    function requestWithdrawl(uint256 _amount) external {
+        if (s_userBalances[msg.sender] < _amount) {
+            revert CollateralInterfaceError_InsufficientCollateralBalance();
+        }
+        sendMessage(msg.sender, _amount, WITHDRAW);
     }
-    function sendMessagePayLINK(
-        uint64 _destinationChainSelector,
-        address _receiver,
-        string calldata _text
-    )
-        external
-        onlyOwner
-        onlyAllowlistedDestinationChain(_destinationChainSelector)
-        validateReceiver(_receiver)
-        returns (bytes32 messageId)
-    {
-        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage(
-            _receiver,
-            _text,
-            address(s_linkToken)
-        );
+
+    function sendMessage(
+        address _user,
+        uint256 _amount,
+        string memory _txType
+    ) private onlyOwner returns (bytes32 messageId) {
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(s_receivingContractAddress),
+            data: abi.encode(_user, _amount, _txType),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 300_000})
+            ),
+            feeToken: address(s_linkToken)
+        });
 
         IRouterClient router = IRouterClient(this.getRouter());
 
-        uint256 fees = router.getFee(_destinationChainSelector, evm2AnyMessage);
+        uint256 fees = router.getFee(s_receivingChainId, evm2AnyMessage);
 
         if (fees > s_linkToken.balanceOf(address(this)))
             revert NotEnoughBalance(s_linkToken.balanceOf(address(this)), fees);
 
         s_linkToken.approve(address(router), fees);
 
-        messageId = router.ccipSend(_destinationChainSelector, evm2AnyMessage);
+        messageId = router.ccipSend(s_receivingChainId, evm2AnyMessage);
 
-        messagesInfo[messageId].status = MessageStatus.Sent;
-
-        emit MessageSent(
-            messageId,
-            _destinationChainSelector,
-            _receiver,
-            _text,
-            address(s_linkToken),
-            fees
+        s_transactionsInfo[messageId] = Transaction(
+            _txType,
+            _user,
+            _amount,
+            MessageStatus.Sent,
+            bytes32(0)
         );
+
+        emit MessageSent(messageId, _amount, msg.sender, _txType);
         return messageId;
     }
 
     function _ccipReceive(
         Client.Any2EVMMessage memory any2EvmMessage
-    )
-        internal
-        override
-        onlyAllowlisted(
-            any2EvmMessage.sourceChainSelector,
-            abi.decode(any2EvmMessage.sender, (address))
-        )
-    {
-        bytes32 initialMsgId = abi.decode(any2EvmMessage.data, (bytes32)); // Decode the data sent by the receiver
+    ) internal override {
+        (bytes32 initialMsgId, string memory status) = abi.decode(
+            any2EvmMessage.data,
+            (bytes32, string)
+        );
         bytes32 acknowledgerMsgId = any2EvmMessage.messageId;
-        messagesInfo[initialMsgId].acknowledgerMessageId = acknowledgerMsgId; // Store the messageId of the received message
+        s_transactionsInfo[initialMsgId]
+            .acknowledgerMessageId = acknowledgerMsgId;
+        Transaction memory txn = s_transactionsInfo[initialMsgId];
+        if (compareStrings(status, DEPOSITSUCCESS)) {
+            s_userBalances[txn.user] += txn.amount;
+            emit DepositSuccess(initialMsgId, txn.user, txn.amount);
+        } else if (compareStrings(status, DEPOSITFAIL)) {
+            s_failedDeposits[txn.user] += txn.amount;
+            emit DepositFailed(initialMsgId, txn.user, txn.amount);
+        } else if (compareStrings(status, WITHDRAWSUCCESS)) {
+            s_acceptedWithdrawls[txn.user] += txn.amount;
+            emit WithdrawRequestSuccess(initialMsgId, txn.user, txn.amount);
+        } else if (compareStrings(status, WITHDRAWFAIL)) {
+            emit WithdrawRequestFailed(initialMsgId, txn.user, txn.amount);
+        }
 
-        if (messagesInfo[initialMsgId].status == MessageStatus.Sent) {
-            messagesInfo[initialMsgId].status = MessageStatus
+        if (s_transactionsInfo[initialMsgId].status == MessageStatus.Sent) {
+            s_transactionsInfo[initialMsgId].status = MessageStatus
                 .ProcessedOnDestination;
-            emit MessageProcessedOnDestination(
-                acknowledgerMsgId,
-                initialMsgId,
-                any2EvmMessage.sourceChainSelector,
-                abi.decode(any2EvmMessage.sender, (address))
-            );
+            emit MessageProcessedOnDestination(acknowledgerMsgId, status);
         } else if (
-            messagesInfo[initialMsgId].status ==
+            s_transactionsInfo[initialMsgId].status ==
             MessageStatus.ProcessedOnDestination
         ) {
             revert MessageHasAlreadyBeenProcessedOnDestination(initialMsgId);
@@ -165,22 +207,6 @@ contract MessageTracker is CCIPReceiver, OwnerIsCreator {
         }
     }
 
-    function _buildCCIPMessage(
-        address _receiver,
-        string calldata _text,
-        address _feeTokenAddress
-    ) private pure returns (Client.EVM2AnyMessage memory) {
-        return
-            Client.EVM2AnyMessage({
-                receiver: abi.encode(_receiver), // ABI-encoded receiver address
-                data: abi.encode(_text), // ABI-encoded string
-                tokenAmounts: new Client.EVMTokenAmount[](0), // Empty array as no tokens are transferred
-                extraArgs: Client._argsToBytes(
-                    Client.EVMExtraArgsV1({gasLimit: 300_000})
-                ),
-                feeToken: _feeTokenAddress
-            });
-    }
     function withdrawToken(
         address _beneficiary,
         address _token
@@ -190,5 +216,30 @@ contract MessageTracker is CCIPReceiver, OwnerIsCreator {
         if (amount == 0) revert NothingToWithdraw();
 
         IERC20(_token).safeTransfer(_beneficiary, amount);
+    }
+
+    function withdrawCollateral(uint256 _amount) external {
+        if (s_acceptedWithdrawls[msg.sender] < _amount) {
+            revert CollateralInterfaceError_InsufficientCollateralBalance();
+        }
+        s_collateralToken.transfer(msg.sender, _amount);
+        emit CollateralWithdrawn(msg.sender, _amount);
+    }
+
+    function withdrawFailedDeposits() external {
+        uint256 amount = s_failedDeposits[msg.sender];
+        if (amount < 0) {
+            revert CollateralInterfaceError_InsufficientCollateralBalance();
+        }
+        s_collateralToken.transfer(msg.sender, amount);
+        emit FailedDepositWithdraw(msg.sender, amount);
+    }
+
+    function compareStrings(
+        string memory a,
+        string memory b
+    ) internal pure returns (bool eql) {
+        eql = (keccak256(abi.encodePacked(a)) ==
+            keccak256(abi.encodePacked(b)));
     }
 }
--- src/CollateralTeller.sol
+++ src/CollateralTeller.sol
@@ -4,17 +4,18 @@
  * @title CollateralTeller
  * @author Karthikeya Gundumogula
  * @notice This contract is the receiver end of the CCIP interface that deposits collateral
- * @dev This contract updates the Users collateral balnce in the HeadStation contract
+ * @dev This contract updates the Users collateral balance in the HeadStation contract
  * after receiving the msg from the sender contract
  */
-pragma solidity ^0.8.20;
 
-import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
-import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
-import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
-import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
-import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
-import {EnumerableMap} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/utils/structs/EnumerableMap.sol";
+pragma solidity 0.8.19;
+
+import {IRouterClient} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/libraries/Client.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {IERC20} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@chainlink/contracts-ccip@1.4.0/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface HeadStation {
     function depositCollateral(
@@ -42,9 +43,12 @@ interface HeadStation {
     ) external;
 }
 
-contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
-    using EnumerableMap for EnumerableMap.Bytes32ToUintMap;
+using SafeERC20 for IERC20;
 
+contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+    error NothingToWithdraw();
+    error CollateralTellerError_TransactionAlreadyProcessed();
     error CollateralTellerError_UnAuthorizedOperation();
     error CollateralTellerError_InSufficientBalance();
     error CollateralTellerError_PausedWithDrawls();
@@ -52,20 +56,20 @@ contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
     error CollateralTellerError_CollateralTransferFailed();
     error CollateralTellerError_AmountOverFlown();
 
-    enum ErrorCode {
-        RESOLVED,
-        FAILED
+    enum TransactionState {
+        RECEIVED,
+        PROCESSED
     }
 
-    struct FailedMessage {
-        bytes32 messageId;
-        ErrorCode errorCode;
+    struct Transaction {
+        string txType;
+        address user;
+        uint amount;
+        TransactionState state;
     }
 
     IERC20 private s_linkToken;
 
-    address public s_receiverContract;
-    uint64 s_destinationChainSelector;
     uint256 private constant HEADSTATIONPRECISION = 10 ** 27;
     string private constant WITHDRAW = "W";
     string private constant DEPOSIT = "D";
@@ -73,11 +77,14 @@ contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
     string private constant DEPOSITFAIL = "DF";
     string private constant WITHDRAWSUCCESS = "WS";
     string private constant WITHDRAWFAIL = "WF";
+    address public s_receiverContract;
+    uint64 s_destinationChainId;
+    bytes32[] private s_pendingTransactionIds;
     bytes32 private s_collateralType;
     HeadStation public s_headStation;
-    uint256 private s_collateralDecimals;
     bool public s_status;
-    EnumerableMap.Bytes32ToUintMap internal s_failedMessages;
+    mapping(bytes32 receivingMessageId => Transaction txn)
+        public s_pendingTransactions;
 
     //--Events--//
     event AuthorizedAddressAdded(address user);
@@ -86,98 +93,115 @@ contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
     event CollateralTokenWithdrawn(address user, uint amount);
     event StausUpdated(bool status);
     event MessageFailed(bytes32 indexed messageId, bytes reason);
+    string private s_lastReceivedText;
 
-    constructor(
-        address _headStation,
-        bytes32 _collateralType,
-        address _router
-    ) CCIPReceiver(_router) {
-        s_status = true;
-        s_headStation = HeadStation(_headStation);
-        s_collateralType = _collateralType;
-        emit AuthorizedAddressAdded(msg.sender);
+    event AcknowledgmentSent(
+        bytes32 indexed acknowledgingMessageId,
+        bytes32 indexed responseMessageId
+    );
+    event TransactionReceived(
+        bytes32 indexed receivingMessageId,
+        address user,
+        uint256 amount,
+        string txType
+    );
+
+    constructor(address _router, address _link) CCIPReceiver(_router) {
+        s_linkToken = IERC20(_link);
     }
 
-    //--Authorization & Adminstration--//
-    modifier authenticate() {
-        if (msg.sender == address(this)) {
-            revert CollateralTellerError_UnAuthorizedOperation();
-        }
-        _;
-    }
-
-    function changeStatus() external authenticate {
-        s_status = !s_status;
-        emit StausUpdated(s_status);
-    }
-
-    function initSenderContract(
-        address _senderContract,
-        uint64 _destinationChainId
+    function initRecevingContract(
+        address _receiver,
+        uint64 _receiverChainId
     ) external onlyOwner {
-        s_destinationChainSelector = _destinationChainId;
-        s_receiverContract = _senderContract;
+        s_receiverContract = _receiver;
+        s_destinationChainId = _receiverChainId;
     }
 
-    /**
-     * @param any2EvmMessage  it is called by the ccip router
-     */
-
-    function ccipReceive(
-        Client.Any2EVMMessage calldata any2EvmMessage
-    ) external override onlyRouter {
-        try this.processMessage(any2EvmMessage) {} catch (bytes memory err) {
-            s_failedMessages.set(
-                any2EvmMessage.messageId,
-                uint256(ErrorCode.FAILED)
-            );
-            emit MessageFailed(any2EvmMessage.messageId, err);
-            return;
+    function executePendingTransaction(bytes32 _messageId) external {
+        Transaction memory txn = s_pendingTransactions[_messageId];
+        if (txn.state == TransactionState.PROCESSED) {
+            revert CollateralTellerError_TransactionAlreadyProcessed();
+        }
+        if (_compareStrings(txn.txType, DEPOSIT)) {
+            bool success = _depositCollateral(txn.user, txn.amount);
+            if (success) _sendStatus(_messageId, DEPOSITSUCCESS);
+            else _sendStatus(_messageId, DEPOSITFAIL);
+        } else {
+            bool success = _withdrawCollateral(txn.user, txn.amount);
+            if (success) _sendStatus(_messageId, WITHDRAWSUCCESS);
+            else _sendStatus(_messageId, WITHDRAWFAIL);
         }
     }
 
-    function processMessage(
-        Client.Any2EVMMessage calldata any2EvmMessage
-    ) external authenticate {
-        _ccipReceive(any2EvmMessage);
+    function _sendStatus(
+        bytes32 _messageIdToAcknowledge,
+        string memory status
+    ) private {
+        Client.EVM2AnyMessage memory acknowledgment = Client.EVM2AnyMessage({
+            receiver: abi.encode(s_receiverContract),
+            data: abi.encode(_messageIdToAcknowledge, status),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000})
+            ),
+            feeToken: address(s_linkToken)
+        });
+        IRouterClient router = IRouterClient(this.getRouter());
+
+        uint256 fees = router.getFee(s_destinationChainId, acknowledgment);
+
+        if (fees > s_linkToken.balanceOf(address(this))) {
+            revert NotEnoughBalance(s_linkToken.balanceOf(address(this)), fees);
+        }
+        s_linkToken.approve(address(router), fees);
+
+        bytes32 messageId = router.ccipSend(
+            s_destinationChainId,
+            acknowledgment
+        );
+        emit AcknowledgmentSent(_messageIdToAcknowledge, messageId);
     }
 
     function _ccipReceive(
         Client.Any2EVMMessage memory any2EvmMessage
     ) internal override {
-        (string memory txType, address user, uint256 amount) = abi.decode(
+        bytes32 messageIdToAcknowledge = any2EvmMessage.messageId;
+        address user;
+        uint256 amount;
+        string memory txType;
+        (user, amount, txType) = abi.decode(
             any2EvmMessage.data,
-            (string, address, uint256)
+            (address, uint, string)
         );
-        if (
-            keccak256(abi.encodePacked(txType)) ==
-            keccak256(abi.encodePacked(DEPOSIT))
-        ) {
-            bool success = _depositCollateral(user, amount);
-            if (success) {
-                _sendCCIPMessage(DEPOSITSUCCESS, any2EvmMessage.messageId);
-            } else {
-                _sendCCIPMessage(DEPOSITFAIL, any2EvmMessage.messageId);
-            }
+        Transaction storage txn = s_pendingTransactions[messageIdToAcknowledge];
+        if (_compareStrings(txType, DEPOSIT)) {
+            txn.txType = DEPOSIT;
+        } else {
+            txn.txType = WITHDRAW;
         }
-        if (
-            keccak256(abi.encodePacked(txType)) ==
-            keccak256(abi.encodePacked(WITHDRAW))
-        ) {
-            bool success = _withdrawCollateral(user, amount);
-            if (success) {
-                _sendCCIPMessage(WITHDRAWSUCCESS, any2EvmMessage.messageId);
-            } else {
-                _sendCCIPMessage(WITHDRAWFAIL, any2EvmMessage.messageId);
-            }
-        }
+        txn.amount = amount;
+        txn.state = TransactionState.RECEIVED;
+        txn.user = user;
+        s_pendingTransactionIds.push(messageIdToAcknowledge);
+        emit TransactionReceived(messageIdToAcknowledge, user, amount, txType);
     }
 
-    //--Deposit & Withdraw Collateral Token--//
+    function withdrawToken(
+        address _beneficiary,
+        address _token
+    ) external onlyOwner {
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+
+        if (amount == 0) revert NothingToWithdraw();
+
+        IERC20(_token).safeTransfer(_beneficiary, amount);
+    }
+
     function _depositCollateral(
         address _user,
         uint _amount
-    ) internal authenticate returns (bool success) {
+    ) private returns (bool success) {
         success = false;
         if (s_status == false) {
             revert CollateralTellerError_PausedWithDrawls();
@@ -193,7 +217,7 @@ contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
     function _withdrawCollateral(
         address _user,
         uint _amount
-    ) internal authenticate returns (bool success) {
+    ) private returns (bool success) {
         success = false;
         if (_amount > type(uint256).max) {
             revert CollateralTellerError_AmountOverFlown();
@@ -203,45 +227,11 @@ contract CollateralTeller is CCIPReceiver, OwnerIsCreator {
         emit CollateralTokenWithdrawn(_user, _amount);
     }
 
-    function _sendCCIPMessage(
-        string memory _status,
-        bytes32 _acknowledgingMessageId
-    ) internal returns (bytes32 messageId, uint256 fees) {
-        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
-            receiver: abi.encode(s_receiverContract),
-            data: abi.encode(_status, _acknowledgingMessageId),
-            tokenAmounts: new Client.EVMTokenAmount[](0),
-            extraArgs: Client._argsToBytes(
-                Client.EVMExtraArgsV1({gasLimit: 400_000})
-            ),
-            feeToken: address(s_linkToken)
-        });
-        IRouterClient router = IRouterClient(this.getRouter());
-        fees = router.getFee(s_destinationChainSelector, evm2AnyMessage);
-        if (fees > s_linkToken.balanceOf(address(this)))
-            revert CollateralTellerError_InSufficientBalance();
-        s_linkToken.approve(address(router), fees);
-        messageId = router.ccipSend(s_destinationChainSelector, evm2AnyMessage);
-    }
-
-    function getFailedMessages(
-        uint256 offset,
-        uint256 limit
-    ) external view returns (FailedMessage[] memory) {
-        uint256 length = s_failedMessages.length();
-        uint256 returnLength = (offset + limit > length)
-            ? length - offset
-            : limit;
-        FailedMessage[] memory failedMessages = new FailedMessage[](
-            returnLength
-        );
-
-        for (uint256 i = 0; i < returnLength; i++) {
-            (bytes32 messageId, uint256 errorCode) = s_failedMessages.at(
-                offset + i
-            );
-            failedMessages[i] = FailedMessage(messageId, ErrorCode(errorCode));
-        }
-        return failedMessages;
+    function _compareStrings(
+        string memory a,
+        string memory b
+    ) internal pure returns (bool eql) {
+        eql = (keccak256(abi.encodePacked(a)) ==
+            keccak256(abi.encodePacked(b)));
     }
 }
